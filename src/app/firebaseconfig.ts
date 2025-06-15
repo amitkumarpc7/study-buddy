0a826056b79d684c69526c7b18730142deb80c33
amitkumarpc7 <amitkpc11@gmail.com>
2025-06-15 14:41:33 +0530

    updated env

--- src/app/firebaseconfig.ts
+++ src/app/firebaseconfig.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNpmb621bX-VMn94SAd_-I_NjwrGzN1mc",
-  authDomain: "studybuddy-814f5.firebaseapp.com",
-  projectId: "studybuddy-814f5",
-  storageBucket: "studybuddy-814f5.firebasestorage.app",
-  messagingSenderId: "709043838497",
-  appId: "1:709043838497:web:60e3271c404db2572acd60",
-  measurementId: "G-N60CSW06R5",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
